32fae3532e1dec22692441386a098d21932f01fd
³ʳᵈ <60966545+TheReal3rd@users.noreply.github.com>
2023-06-21 11:27:50 +0100

    Added Intro and animations and other things

--- main.py
+++ main.py
@@ -32,15 +32,16 @@ def collision():
             sprites.destroy(value)
             continue
         # Step 2 Check whether the sprites a close too each other - if not skip
-        dist = calcDist(value.x, value.y, PlayerOne.x, PlayerOne.y)
+        dist = calcDist(value.x, value.y, playerOne.x, playerOne.y)
         if dist >= 10:
             continue
         # Step 3 Check for collision
-        if PlayerOne.overlaps_with(value):
+        if playerOne.overlaps_with(value):
             value.start_effect(effects.fire, 100)
             sprites.destroy(value)
             info.change_life_by(-1)
             screenFlash = True
+            screenFlashTimer.reset()
     # Player Bullets This is the same as the first for loop but for the player attacking the enemy
     for value1 in sprites.all_of_kind(SpriteKind.projectile):
         dist = calcDist(value1.x,value1.y,scene.screen_width() / 2,scene.screen_height() / 2)
@@ -48,89 +49,122 @@ def collision():
             # # Off screen remove it.
             sprites.destroy(value1)
             continue
-        dist = calcDist(value1.x, value1.y, EnemyOne.x, EnemyOne.y)
+        dist = calcDist(value1.x, value1.y, enemyOne.x, enemyOne.y)
         if dist >= 10:
             continue
-        if EnemyOne.overlaps_with(value1):
+        if enemyOne.overlaps_with(value1):
             value1.start_effect(effects.fire, 100)
             sprites.destroy(value1)
             enemyHealth += 0 - 1
-# Movement Function.
-# 
-# This block of code gets executed by the game loop its only job is to check what keys are pressed if a certain key is pressed run.
-def movement():
-    global moveSpeed, fireType
+
+# PlayerUpdate this will only contain code related to the player.
+def updatePlayer():
+    global moveSpeed, fireType, fireDelay, screenFlash, screenFlashTimer
+    #Slow move switch
     if controller.B.is_pressed():
-        moveSpeed = 1
+        moveSpeed = 1##TODO add a foreward facing shield.
     else:
         moveSpeed = 2
+    #Movement
     if controller.down.is_pressed():
-        PlayerOne.y += moveSpeed
+        playerOne.y += moveSpeed
     if controller.up.is_pressed():
-        PlayerOne.y += moveSpeed * -1
+        playerOne.y += moveSpeed * -1
     if controller.left.is_pressed():
-        PlayerOne.x += moveSpeed * -1
+        playerOne.x += moveSpeed * -1
     if controller.right.is_pressed():
-        PlayerOne.x += moveSpeed
-# This updates the enemy the stage system is here to add more enemies.
-def updateEnemy():
-    global waypoint, EnemyStage, EnemyOne, fireType
-    if EnemyStage == 0:
-        if enemyFireDelay.passed(2000):
-            # # if 2000 ms passed the enemy will shoot.
-            angle = 60
-            if Math.percent_chance(50):
-                angle = -60
-            # 1 is the lowest.
-            # 30 max
-            shootBullets(EnemyOne.x, EnemyOne.y, 15, angle, fireType, 4)
-            enemyFireDelay.reset()
-        elif Math.percent_chance(2):
-            # # 2% chance to generate a new waypoint.
-            if waypoint == None:
-                tPosX = Math.round(Math.random() * 100)
-                tPosY = Math.round(Math.random() * 100)
-                # Max check
-                tPosX = min(tPosX, scene.screen_width())
-                tPosY = min(tPosY, scene.screen_height())
-                # #Min check
-                tPosX = max(tPosX, 0)
-                tPosY = max(tPosY, 0)
-                distToPlayer = calcDist(tPosX, tPosY, PlayerOne.x, PlayerOne.y)
-                if distToPlayer >= 60:
-                    waypoint = [tPosX, tPosY]
-        elif Math.percent_chance(0.001):
-            if fireType == 0:
-                fireType = 1
-            else:
-                fireType = 0
-    elif EnemyStage == 1:
-        pass
-    else:
-        game.set_game_over_message(True, "GAME OVER!")
-    if waypoint != None:
-        # # IF we have a waypoint we move towards it.
-        # X
-        if EnemyOne.x < waypoint[0]:
-            EnemyOne.x += 1
-        elif EnemyOne.x > waypoint[0]:
-            EnemyOne.x -= 1
-        # Y
-        if EnemyOne.y < waypoint[1]:
-            EnemyOne.y += 1
-        elif EnemyOne.y > waypoint[1]:
-            EnemyOne.y -= 1
-        if Math.round(calcDist(EnemyOne.x, EnemyOne.y, waypoint[0], waypoint[1])) <= 1:
-            waypoint = None
-    if enemyHealth <= 0:
-        EnemyStage = 1
-        sprites.destroy(EnemyOne)
-# Players shooting code.
-def shoot():
+        playerOne.x += moveSpeed
+    #Shooting
     if controller.A.is_pressed():
         if fireDelay.passed(500):
-            projectile2 = sprites.create_projectile_from_sprite(assets.image("""PlayerBullet"""), PlayerOne, 0, -50)
+            projectile2 = sprites.create_projectile_from_sprite(assets.image("""PlayerBullet"""), playerOne, 0, -50)
             fireDelay.reset()
+    if screenFlash:
+        scene.set_background_color(2)
+        if screenFlashTimer.passed(200):
+            scene.set_background_color(0)
+            screenFlash = False
+
+
+# This updates the enemy the stage system is here to add more enemies.
+def updateEnemy():
+    global waypoint, enemyStage, enemyOne, fireType, enemyHealth, enemyNormalImage, intro
+    info.set_score(enemyHealth)
+    if not intro:
+        if enemyStage == 0:
+            if enemyFireDelay.passed(2000):
+                # # if 2000 ms passed the enemy will shoot.
+                angle = 60
+                if Math.percent_chance(50):
+                    angle = -60
+                # 1 is the lowest.
+                # 30 max
+                shootBullets(enemyOne.x, enemyOne.y, 15, angle, fireType, 4)
+                enemyOne.set_image(assets.image("""EnemyShoot"""))
+                enemyAnimDelay.reset()
+                enemyFireDelay.reset()
+            elif Math.percent_chance(2):
+                # 2% chance to generate a new waypoint.
+                if waypoint == None:
+                    tPosX = Math.round(Math.random() * 100)
+                    tPosY = Math.round(Math.random() * 100)
+                    # Min check
+                    tPosX = min(tPosX, scene.screen_width())
+                    tPosY = min(tPosY, scene.screen_height())
+                    # Max check
+                    tPosX = max(tPosX, 0)
+                    tPosY = max(tPosY, 0)
+                    distToPlayer = calcDist(tPosX, tPosY, playerOne.x, playerOne.y)
+                    if distToPlayer >= 60:
+                        waypoint = [tPosX, tPosY]
+            elif Math.percent_chance(0.001):
+                if fireType == 0:
+                    fireType = 1
+                else:
+                    fireType = 0
+            else:
+                if(enemyAnimDelay.passed(500)):
+                    enemyOne.set_image(enemyNormalImage)
+        elif enemyStage == 1:
+            pass
+        else:
+            game.set_game_over_message(True, "GAME OVER!")
+
+    if waypoint != None:
+        # # IF we have a waypoint we move towards it.
+        setImage = False
+        # X
+        if enemyOne.x < waypoint[0]:
+            enemyOne.x += 1
+            if not setImage:
+                enemyNormalImage = assets.image("""EnemyRight""")
+                setImage = True
+        elif enemyOne.x > waypoint[0]:
+            enemyOne.x -= 1
+            if not setImage:
+                enemyNormalImage = assets.image("""EnemyLeft""")
+                setImage = True
+        # Y
+        if enemyOne.y < waypoint[1]:
+            enemyOne.y += 1
+            if not setImage:
+                enemyNormalImage = assets.image("""EnemyNormal""")
+                setImage = True
+        elif enemyOne.y > waypoint[1]:
+            enemyOne.y -= 1
+            if not setImage:
+                enemyNormalImage = assets.image("""EnemyNormal""")
+                setImage = True
+
+        if Math.round(calcDist(enemyOne.x, enemyOne.y, waypoint[0], waypoint[1])) <= 1:
+            enemyNormalImage = assets.image("""EnemyNormal""")
+            waypoint = None
+            intro = False
+
+    if enemyHealth <= 0:
+        enemyStage = 1
+        sprites.destroy(enemyOne)
+
 # Distance Caculation.
 # Provide x1 and y1 and it'll calculate the distance to x2 and y2
 def calcDist(posX: number, posY: number, posX1: number, posY1: number):
@@ -154,7 +188,7 @@ def wrapDegrees(degrees):
         d += 360.0;
     return d;
 
-#Copied from Java Math library due to it not exist in this Math lib 
+#Copied from Java Math library due to it not exist in this Math lib
 def toDegrees(rot):
     return rot * 57.29577951308232
 
@@ -180,52 +214,54 @@ def shootBullets(posX2: number, posY2: number, distance: number, angleOffset: nu
             angle2 += numBullets * 10
     elif typeBullet == 1:
         # Laser Shoot
-        angle = calcAngle(EnemyOne.x - (EnemyOne.width / 2), EnemyOne.y - (EnemyOne.height / 2), PlayerOne.x- (PlayerOne.width / 2), PlayerOne.y - (PlayerOne.height / 2)) * 0.017453292519943295
+        angle = calcAngle(enemyOne.x - (enemyOne.width / 2), enemyOne.y - (enemyOne.height / 2), playerOne.x- (playerOne.width / 2), playerOne.y - (playerOne.height / 2)) * 0.017453292519943295
         for x in range(-6, 6):
             enemyProjectile = sprites.create(assets.image("""LaserPixel"""),SpriteKind.EnemyProjectile)
             if not (angle <= 1.8 and angle >= 0.8  or angle <= -1.8 and angle >= -0.8 ):
                 enemyProjectile.set_position(posX2 + (x * enemyProjectile.width), posY2)
             else:
                 enemyProjectile.set_position(posX2, posY2 + (x * enemyProjectile.height))
-            xVel = Math.sin(angle) * 50
-            yVel = Math.cos(angle) * 50
-            enemyProjectile.set_velocity(xVel, yVel)
+            velX = Math.sin(angle) * 50
+            velY = Math.cos(angle) * 50
+            enemyProjectile.set_velocity(velX, velY)
+    elif typeBullet == 2:
+        pass#TODO add another shooter type.
 
 moveSpeed = 0
 screenFlash = False
-EnemyStage = 0
-PlayerOne: Sprite = None
+playerOne: Sprite = None
 enemyHealth = 0
-EnemyOne: Sprite = None
-waypoint: any = None
+enemyOne: Sprite = None
+waypoint: any = (80, 15)
 fireDelay = msDelay()
 enemyFireDelay = msDelay()
 enemyHealth = 30
-EnemyOne = sprites.create(assets.image("""
-    myImage1
+enemyOne = sprites.create(assets.image("""
+    EnemyNormal
 """), SpriteKind.enemy)
-EnemyOne.set_position(80, 15)
-PlayerOne = sprites.create(assets.image("""
+enemyOne.set_position(80, -15)
+playerOne = sprites.create(assets.image("""
     myImage
 """), SpriteKind.player)
-PlayerOne.set_position(80, 90)
-PlayerOne.set_stay_in_screen(True)
-PlayerOne.set_scale(0.4, ScaleAnchor.MIDDLE)
-EnemyStage = 0
+playerOne.set_position(80, 90)
+playerOne.set_stay_in_screen(True)
+playerOne.set_scale(0.4, ScaleAnchor.MIDDLE)
+enemyStage = 0
 screenFlashTimer = msDelay()
-intro = 1
+enemyNormalImage = assets.image("""EnemyNormal""")
+enemyAnimDelay = msDelay()
+intro = True
 info.set_life(3)
 fireType = 0
-# Game Loop
-# 
-# Block of instructions that must be ran every game tick / frame.
-# 
-# Handle all the logic related to Collision, Movement and Controls.
 
+# Game Loop
+#
+# Block of instructions that must be ran every game tick / frame.
+#
+# Handle all the logic related to Collision, Movement and Controls.
 def on_forever():
     game.stats = True
-    movement()
-    shoot()
+    updatePlayer()
     collision()
     updateEnemy()
 forever(on_forever)
--- main.ts
+++ main.ts
@@ -57,17 +57,18 @@ function collision() {
         }
         
         //  Step 2 Check whether the sprites a close too each other - if not skip
-        dist = calcDist(value.x, value.y, PlayerOne.x, PlayerOne.y)
+        dist = calcDist(value.x, value.y, playerOne.x, playerOne.y)
         if (dist >= 10) {
             continue
         }
         
         //  Step 3 Check for collision
-        if (PlayerOne.overlapsWith(value)) {
+        if (playerOne.overlapsWith(value)) {
             value.startEffect(effects.fire, 100)
             sprites.destroy(value)
             info.changeLifeBy(-1)
             screenFlash = true
+            screenFlashTimer.reset()
         }
         
     }
@@ -80,12 +81,12 @@ function collision() {
             continue
         }
         
-        dist = calcDist(value1.x, value1.y, EnemyOne.x, EnemyOne.y)
+        dist = calcDist(value1.x, value1.y, enemyOne.x, enemyOne.y)
         if (dist >= 10) {
             continue
         }
         
-        if (EnemyOne.overlapsWith(value1)) {
+        if (enemyOne.overlapsWith(value1)) {
             value1.startEffect(effects.fire, 100)
             sprites.destroy(value1)
             enemyHealth += 0 - 1
@@ -94,31 +95,51 @@ function collision() {
     }
 }
 
-//  Movement Function.
-//  
-//  This block of code gets executed by the game loop its only job is to check what keys are pressed if a certain key is pressed run.
-function movement() {
+//  PlayerUpdate this will only contain code related to the player.
+function updatePlayer() {
+    let projectile2: Sprite;
     
+    // Slow move switch
     if (controller.B.isPressed()) {
         moveSpeed = 1
     } else {
+        // #TODO add a foreward facing shield.
         moveSpeed = 2
     }
     
+    // Movement
     if (controller.down.isPressed()) {
-        PlayerOne.y += moveSpeed
+        playerOne.y += moveSpeed
     }
     
     if (controller.up.isPressed()) {
-        PlayerOne.y += moveSpeed * -1
+        playerOne.y += moveSpeed * -1
     }
     
     if (controller.left.isPressed()) {
-        PlayerOne.x += moveSpeed * -1
+        playerOne.x += moveSpeed * -1
     }
     
     if (controller.right.isPressed()) {
-        PlayerOne.x += moveSpeed
+        playerOne.x += moveSpeed
+    }
+    
+    // Shooting
+    if (controller.A.isPressed()) {
+        if (fireDelay.passed(500)) {
+            projectile2 = sprites.createProjectileFromSprite(assets.image`PlayerBullet`, playerOne, 0, -50)
+            fireDelay.reset()
+        }
+        
+    }
+    
+    if (screenFlash) {
+        scene.setBackgroundColor(2)
+        if (screenFlashTimer.passed(200)) {
+            scene.setBackgroundColor(0)
+            screenFlash = false
+        }
+        
     }
     
 }
@@ -129,90 +150,109 @@ function updateEnemy() {
     let tPosX: number;
     let tPosY: number;
     let distToPlayer: number;
+    let setImage: boolean;
     
-    if (EnemyStage == 0) {
-        if (enemyFireDelay.passed(2000)) {
-            //  # if 2000 ms passed the enemy will shoot.
-            angle = 60
-            if (Math.percentChance(50)) {
-                angle = -60
-            }
-            
-            //  1 is the lowest.
-            //  30 max
-            shootBullets(EnemyOne.x, EnemyOne.y, 15, angle, fireType, 4)
-            enemyFireDelay.reset()
-        } else if (Math.percentChance(2)) {
-            //  # 2% chance to generate a new waypoint.
-            if (waypoint == null) {
-                tPosX = Math.round(Math.random() * 100)
-                tPosY = Math.round(Math.random() * 100)
-                //  Max check
-                tPosX = Math.min(tPosX, scene.screenWidth())
-                tPosY = Math.min(tPosY, scene.screenHeight())
-                //  #Min check
-                tPosX = Math.max(tPosX, 0)
-                tPosY = Math.max(tPosY, 0)
-                distToPlayer = calcDist(tPosX, tPosY, PlayerOne.x, PlayerOne.y)
-                if (distToPlayer >= 60) {
-                    waypoint = [tPosX, tPosY]
+    info.setScore(enemyHealth)
+    if (!intro) {
+        if (enemyStage == 0) {
+            if (enemyFireDelay.passed(2000)) {
+                //  # if 2000 ms passed the enemy will shoot.
+                angle = 60
+                if (Math.percentChance(50)) {
+                    angle = -60
                 }
                 
+                //  1 is the lowest.
+                //  30 max
+                shootBullets(enemyOne.x, enemyOne.y, 15, angle, fireType, 4)
+                enemyOne.setImage(assets.image`EnemyShoot`)
+                enemyAnimDelay.reset()
+                enemyFireDelay.reset()
+            } else if (Math.percentChance(2)) {
+                //  2% chance to generate a new waypoint.
+                if (waypoint == null) {
+                    tPosX = Math.round(Math.random() * 100)
+                    tPosY = Math.round(Math.random() * 100)
+                    //  Min check
+                    tPosX = Math.min(tPosX, scene.screenWidth())
+                    tPosY = Math.min(tPosY, scene.screenHeight())
+                    //  Max check
+                    tPosX = Math.max(tPosX, 0)
+                    tPosY = Math.max(tPosY, 0)
+                    distToPlayer = calcDist(tPosX, tPosY, playerOne.x, playerOne.y)
+                    if (distToPlayer >= 60) {
+                        waypoint = [tPosX, tPosY]
+                    }
+                    
+                }
+                
+            } else if (Math.percentChance(0.001)) {
+                if (fireType == 0) {
+                    fireType = 1
+                } else {
+                    fireType = 0
+                }
+                
+            } else if (enemyAnimDelay.passed(500)) {
+                enemyOne.setImage(enemyNormalImage)
             }
             
-        } else if (Math.percentChance(0.001)) {
-            if (fireType == 0) {
-                fireType = 1
-            } else {
-                fireType = 0
-            }
+        } else if (enemyStage == 1) {
             
+        } else {
+            game.setGameOverMessage(true, "GAME OVER!")
         }
         
-    } else if (EnemyStage == 1) {
-        
-    } else {
-        game.setGameOverMessage(true, "GAME OVER!")
     }
     
     if (waypoint != null) {
         //  # IF we have a waypoint we move towards it.
+        setImage = false
         //  X
-        if (EnemyOne.x < waypoint[0]) {
-            EnemyOne.x += 1
-        } else if (EnemyOne.x > waypoint[0]) {
-            EnemyOne.x -= 1
+        if (enemyOne.x < waypoint[0]) {
+            enemyOne.x += 1
+            if (!setImage) {
+                enemyNormalImage = assets.image`EnemyRight`
+                setImage = true
+            }
+            
+        } else if (enemyOne.x > waypoint[0]) {
+            enemyOne.x -= 1
+            if (!setImage) {
+                enemyNormalImage = assets.image`EnemyLeft`
+                setImage = true
+            }
+            
         }
         
         //  Y
-        if (EnemyOne.y < waypoint[1]) {
-            EnemyOne.y += 1
-        } else if (EnemyOne.y > waypoint[1]) {
-            EnemyOne.y -= 1
+        if (enemyOne.y < waypoint[1]) {
+            enemyOne.y += 1
+            if (!setImage) {
+                enemyNormalImage = assets.image`EnemyNormal`
+                setImage = true
+            }
+            
+        } else if (enemyOne.y > waypoint[1]) {
+            enemyOne.y -= 1
+            if (!setImage) {
+                enemyNormalImage = assets.image`EnemyNormal`
+                setImage = true
+            }
+            
         }
         
-        if (Math.round(calcDist(EnemyOne.x, EnemyOne.y, waypoint[0], waypoint[1])) <= 1) {
+        if (Math.round(calcDist(enemyOne.x, enemyOne.y, waypoint[0], waypoint[1])) <= 1) {
+            enemyNormalImage = assets.image`EnemyNormal`
             waypoint = null
+            intro = false
         }
         
     }
     
     if (enemyHealth <= 0) {
-        EnemyStage = 1
-        sprites.destroy(EnemyOne)
-    }
-    
-}
-
-//  Players shooting code.
-function shoot() {
-    let projectile2: Sprite;
-    if (controller.A.isPressed()) {
-        if (fireDelay.passed(500)) {
-            projectile2 = sprites.createProjectileFromSprite(assets.image`PlayerBullet`, PlayerOne, 0, -50)
-            fireDelay.reset()
-        }
-        
+        enemyStage = 1
+        sprites.destroy(enemyOne)
     }
     
 }
@@ -247,7 +287,7 @@ function wrapDegrees(degrees: number): number {
     return d
 }
 
-// Copied from Java Math library due to it not exist in this Math lib 
+// Copied from Java Math library due to it not exist in this Math lib
 function toDegrees(rot: number): number {
     return rot * 57.29577951308232
 }
@@ -266,8 +306,6 @@ function shootBullets(posX2: number, posY2: number, distance: number, angleOffse
     let velY: number;
     let enemyProjectile: Sprite;
     let angle: number;
-    let xVel: number;
-    let yVel: number;
     if (typeBullet == 0) {
         //  Circle shoot.
         angle2 = -180
@@ -283,7 +321,7 @@ function shootBullets(posX2: number, posY2: number, distance: number, angleOffse
         }
     } else if (typeBullet == 1) {
         //  Laser Shoot
-        angle = calcAngle(EnemyOne.x - EnemyOne.width / 2, EnemyOne.y - EnemyOne.height / 2, PlayerOne.x - PlayerOne.width / 2, PlayerOne.y - PlayerOne.height / 2) * 0.017453292519943295
+        angle = calcAngle(enemyOne.x - enemyOne.width / 2, enemyOne.y - enemyOne.height / 2, playerOne.x - playerOne.width / 2, playerOne.y - playerOne.height / 2) * 0.017453292519943295
         for (let x = -6; x < 6; x++) {
             enemyProjectile = sprites.create(assets.image`LaserPixel`, SpriteKind.EnemyProjectile)
             if (!(angle <= 1.8 && angle >= 0.8 || angle <= -1.8 && angle >= -0.8)) {
@@ -292,48 +330,51 @@ function shootBullets(posX2: number, posY2: number, distance: number, angleOffse
                 enemyProjectile.setPosition(posX2, posY2 + x * enemyProjectile.height)
             }
             
-            xVel = Math.sin(angle) * 50
-            yVel = Math.cos(angle) * 50
-            enemyProjectile.setVelocity(xVel, yVel)
+            velX = Math.sin(angle) * 50
+            velY = Math.cos(angle) * 50
+            enemyProjectile.setVelocity(velX, velY)
         }
+    } else if (typeBullet == 2) {
+        
     }
     
 }
 
+// TODO add another shooter type.
 let moveSpeed = 0
 let screenFlash = false
-let EnemyStage = 0
-let PlayerOne : Sprite = null
+let playerOne : Sprite = null
 let enemyHealth = 0
-let EnemyOne : Sprite = null
-let waypoint : number[] = null
+let enemyOne : Sprite = null
+let waypoint = [80, 15]
 let fireDelay = new msDelay()
 let enemyFireDelay = new msDelay()
 enemyHealth = 30
-EnemyOne = sprites.create(assets.image`
-    myImage1
+enemyOne = sprites.create(assets.image`
+    EnemyNormal
 `, SpriteKind.Enemy)
-EnemyOne.setPosition(80, 15)
-PlayerOne = sprites.create(assets.image`
+enemyOne.setPosition(80, -15)
+playerOne = sprites.create(assets.image`
     myImage
 `, SpriteKind.Player)
-PlayerOne.setPosition(80, 90)
-PlayerOne.setStayInScreen(true)
-PlayerOne.setScale(0.4, ScaleAnchor.Middle)
-EnemyStage = 0
+playerOne.setPosition(80, 90)
+playerOne.setStayInScreen(true)
+playerOne.setScale(0.4, ScaleAnchor.Middle)
+let enemyStage = 0
 let screenFlashTimer = new msDelay()
-let intro = 1
+let enemyNormalImage = assets.image`EnemyNormal`
+let enemyAnimDelay = new msDelay()
+let intro = true
 info.setLife(3)
 let fireType = 0
 //  Game Loop
-//  
+// 
 //  Block of instructions that must be ran every game tick / frame.
-//  
+// 
 //  Handle all the logic related to Collision, Movement and Controls.
 forever(function on_forever() {
     game.stats = true
-    movement()
-    shoot()
+    updatePlayer()
     collision()
     updateEnemy()
 })
